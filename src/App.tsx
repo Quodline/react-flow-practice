66e6d4b77fd0a55ed4991c3fb86eab587e77b9e5
Ore Adeleye <oreadeleye67@gmail.com>
2023-11-08 12:39:32 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ import ReactFlow, {
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import './App.css';
-import {useCallback} from 'react'
+import {useCallback} from 'react';
 
 const initialNodes = [
     {id: '1', position: {x: 0, y: 0}, data: {label: '1'}},
-    {id: '2', position: {x: 0, y: 100}, data: {label: '2'}},
+    {id: '2', position: {x: 100, y: 100}, data: {label: '2'}},
 ];
-const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
+const initialEdges = [{id: 'e1-2', source: '1', target: '2', animated: true}];
 
 function App() {
     const [nodes, , onNodesChange] = useNodesState(initialNodes);
@@ -35,8 +35,10 @@ function App() {
                 onNodesChange={onNodesChange}
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
+                defaultEdgeOptions={{animated: true}}
+                fitView
             >
-
+                <button onClick={() => setEdges([])}>Remove all edges</button>
                 <Controls />
                 <MiniMap />
                 <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
